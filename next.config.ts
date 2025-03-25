76afcc79f5a412706ef7922f8f4398dcca9cb6f7
Ron <ronbarak2000@gmail.com>
2025-03-25 13:31:23 +0200

    back to build

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  images: { unoptimized: true },
 };
 
 export default nextConfig;
