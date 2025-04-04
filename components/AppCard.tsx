3f99a25b54d0f0902416c694ccfca328160a5a97
Ron Barak <ronbarak2000@gmail.com>
2025-04-04 19:18:29 +0300

    added title, description and technologies to app-card

--- components/AppCard.tsx
+++ components/AppCard.tsx
@@ -1,26 +1,55 @@
 import Image from "next/image";
+import { PiCursorClickLight } from "react-icons/pi";
 
 interface AppCardProps {
   imgSrc: string;
+  title: string;
+  description: string;
+  technologies: Array<string>;
   href?: string;
 }
 
-const AppCard: React.FC<AppCardProps> = ({ imgSrc, href }) => {
-  const handleImageClick = () => {
+const AppCard: React.FC<AppCardProps> = ({
+  imgSrc,
+  href,
+  title,
+  description,
+  technologies,
+}) => {
+  const handleClick = () => {
     if (!href) return;
     window.open(href, "_blank")?.focus();
   };
 
   return (
     <div className="app-card">
-      <Image
-        width={540}
-        height={270}
-        src={imgSrc}
-        alt=""
-        className="cursor-pointer rounded-xl"
-        onClick={handleImageClick}
-      />
+      <div className="app-card-info">
+        <p className="app-card-title" onClick={handleClick}>
+          <span className="relative">
+            {title}
+            <PiCursorClickLight className="absolute right-[-17px] top-4" />
+          </span>
+        </p>
+        <div className="app-card-description">
+          <span dangerouslySetInnerHTML={{ __html: description }} />
+        </div>
+        <p className="mt-1.5 font-medium">Technologies:</p>
+        <ul className="app-card-technologies">
+          {technologies.map((technology) => (
+            <li key={technology}>{technology}.</li>
+          ))}
+        </ul>
+      </div>
+      <div className="app-card-image-container">
+        <Image
+          width={540}
+          height={270}
+          src={imgSrc}
+          alt=""
+          className="cursor-pointer rounded-xl"
+          onClick={handleClick}
+        />
+      </div>
     </div>
   );
 };
--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -50,31 +50,122 @@ const Features = () => {
           className="flex flex-wrap justify-center gap-10"
         >
           <AppCard
-            imgSrc="/img/recipes-and-ingredients.png"
-            href="https://recipes-and-ingredients.vercel.app/"
-          />
-          <AppCard
-            imgSrc="/img/everx-algoplanner.png"
-            href="https://www.linkedin.com/company/ever-x/"
-          />
-          <AppCard
-            imgSrc="/img/fastjob.png"
-            href="https://fastjob.vercel.app/"
-          />
-          <AppCard
-            imgSrc="/img/outerz.jpg"
-            href="https://github.com/ron1232/outerz/"
-          />
-          <AppCard
-            imgSrc="/img/outerx.jpg"
-            href="https://github.com/ron1232/outerx"
+            imgSrc="/img/enter-catalog.png"
+            title="Enter Catalog"
+            description="A users catalog for the company ENTER, which provides a professional, supportive and promoting space for people with employment barriers."
+            technologies={[
+              "Angular",
+              "Angular Material",
+              "SASS",
+              "Nest.js",
+              "TypeScript",
+              "PostgreSQL",
+              "JWT & Passport",
+            ]}
           />
           <AppCard
             imgSrc="/img/pitangoux.png"
             href="https://www.pitangoux.com/"
+            title="PitangoUX"
+            description="PitangoUX is an end-to-end UX/UI service agency that I used to work for as a Full Stack Developer."
+            technologies={[
+              "React",
+              "Redux",
+              "Zustand",
+              "Next.js",
+              "Vue.js",
+              "Laravel",
+              "Express",
+              "Nest.js",
+              "TypeScript",
+              "JavaScript",
+              "PHP",
+              "MongoDB",
+              "MySQL",
+            ]}
           />
-          <AppCard imgSrc="/img/yna.png" href="https://www.yna.co.il/" />
-          <AppCard imgSrc="/img/enter-catalog.png" />
+          <AppCard
+            imgSrc="/img/yna.png"
+            href="https://www.yna.co.il/"
+            title="Y&A"
+            description="A software company that I used to work for as a Full Stack Developer."
+            technologies={[
+              "React",
+              "Redux",
+              "Zustand",
+              "Next.js",
+              "Vue.js",
+              "Laravel",
+              "Express",
+              "Nest.js",
+              "TypeScript",
+              "JavaScript",
+              "PHP",
+              "MongoDB",
+              "MySQL",
+            ]}
+          />
+          <AppCard
+            imgSrc="/img/recipes-and-ingredients.png"
+            href="https://recipes-and-ingredients.vercel.app/"
+            title="Recipes & Ingredients"
+            description="A site where you could post your own recipes using our ingredients, also supports fetching recipes from a youtube video."
+            technologies={[
+              "Next.js 15",
+              "React",
+              "TypeScript",
+              "Sanity",
+              "TailwindCSS",
+              "Zustand",
+              "Clerk",
+            ]}
+          />
+          <AppCard
+            imgSrc="/img/everx-algoplanner.png"
+            href="https://www.linkedin.com/company/ever-x/"
+            title="AlgoPlanner"
+            description="EverX (formerly known as AlgoPlanner) is a platform for supply chain planning teams to plan, build and customize automations."
+            technologies={[
+              "Electron",
+              "React",
+              "Nest.js",
+              "TypeScript",
+              "DynamoDB",
+              "Material UI",
+              "TailwindCSS",
+              "SASS",
+            ]}
+          />
+          <AppCard
+            imgSrc="/img/fastjob.png"
+            href="https://fastjob.vercel.app/"
+            title="Fast Job"
+            description="A platform where you can post your own jobs, or apply for existing ones."
+            technologies={[
+              "Next.js",
+              "React",
+              "Nest.js",
+              "TypeScript",
+              "MongoDB",
+              "Material UI",
+              "TailwindCSS",
+              "SASS",
+            ]}
+          />
+          <AppCard
+            imgSrc="/img/outerz.jpg"
+            href="https://github.com/ron1232/outerz/"
+            title="OuterZ"
+            description="A small ecommerce site that I built back in 2021."
+            technologies={["React", "Bootstrap", "Redux", "Express.js", "JWT"]}
+          />
+          <AppCard
+            imgSrc="/img/outerx.jpg"
+            href="https://github.com/ron1232/outerx"
+            title="OuterX"
+            description="A small ecommerce site that I built back in 2021."
+            technologies={["Laravel", "PHP", "MySQL"]}
+          />
         </div>
       </div>
     </div>
