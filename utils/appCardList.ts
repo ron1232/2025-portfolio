0944aaea548903f6b5c6a47a6aeec87705256adc
Ron Barak <ronbarak2000@gmail.com>
2025-04-25 20:47:19 +0300

    moved AppCard data to a file 'appCardList'

---
+++ utils/appCardList.ts
@@ -0,0 +1,143 @@
+type AppCardListItem = Array<{
+  id: number;
+  imgSrc: string;
+  href: string;
+  title: string;
+  description: string;
+  technologies: Array<string>;
+}>;
+
+export const appCardList: AppCardListItem = [
+  {
+    id: 1,
+    imgSrc: "/img/pitangoux.png",
+    href: "https://www.pitangoux.com/",
+    title: "PitangoUX",
+    description:
+      "PitangoUX is an end-to-end UX/UI service agency that I used to work for as a Full Stack Developer.",
+    technologies: [
+      "React",
+      "Redux",
+      "Zustand",
+      "Next.js",
+      "Vue.js",
+      "Laravel",
+      "Express",
+      "Nest.js",
+      "TypeScript",
+      "JavaScript",
+      "PHP",
+      "MongoDB",
+      "MySQL",
+    ],
+  },
+  {
+    id: 2,
+    imgSrc: "/img/yna.png",
+    href: "https://www.yna.co.il/",
+    title: "Y&A",
+    description:
+      "A software company that I used to work for as a Full Stack Developer.",
+    technologies: [
+      "React",
+      "Redux",
+      "Zustand",
+      "Next.js",
+      "Vue.js",
+      "Laravel",
+      "Express",
+      "Nest.js",
+      "TypeScript",
+      "JavaScript",
+      "PHP",
+      "MongoDB",
+      "MySQL",
+    ],
+  },
+  {
+    id: 3,
+    imgSrc: "/img/recipes-and-ingredients.png",
+    href: "https://recipes-and-ingredients.vercel.app/",
+    title: "Recipes & Ingredients",
+    description:
+      "A site where you could post your own recipes using our ingredients, also supports fetching recipes from a youtube video.",
+    technologies: [
+      "Next.js 15",
+      "React",
+      "TypeScript",
+      "Sanity",
+      "TailwindCSS",
+      "Zustand",
+      "Clerk",
+    ],
+  },
+  {
+    id: 4,
+    imgSrc: "/img/everx-algoplanner.png",
+    href: "https://www.linkedin.com/company/ever-x/",
+    title: "AlgoPlanner",
+    description:
+      "EverX (formerly known as AlgoPlanner) is a platform for supply chain planning teams to plan, build and customize automations.",
+    technologies: [
+      "Electron",
+      "React",
+      "Nest.js",
+      "TypeScript",
+      "DynamoDB",
+      "Material UI",
+      "TailwindCSS",
+      "SASS",
+    ],
+  },
+  {
+    id: 5,
+    imgSrc: "/img/fastjob.png",
+    href: "https://fastjob.vercel.app/",
+    title: "Fast Job",
+    description:
+      "A platform where you can post your own jobs, or apply for existing ones.",
+    technologies: [
+      "Next.js",
+      "React",
+      "Nest.js",
+      "TypeScript",
+      "MongoDB",
+      "Material UI",
+      "TailwindCSS",
+      "SASS",
+    ],
+  },
+  {
+    id: 6,
+    imgSrc: "/img/enter-catalog.png",
+    href: "https://enter-catalog-frontend.onrender.com/",
+    title: "Enter Catalog",
+    description:
+      "A users catalog of the company ENTER, which provides a professional, supportive and promoting space for people with employment barriers.",
+    technologies: [
+      "Angular",
+      "Angular Material",
+      "SASS",
+      "Nest.js",
+      "TypeScript",
+      "PostgreSQL",
+      "JWT & Passport",
+    ],
+  },
+  {
+    id: 7,
+    imgSrc: "/img/outerz.jpg",
+    href: "https://github.com/ron1232/outerz/",
+    title: "OuterZ",
+    description: "A small ecommerce site that I built back in 2021.",
+    technologies: ["React", "Bootstrap", "Redux", "Express.js", "JWT"],
+  },
+  {
+    id: 8,
+    imgSrc: "/img/outerx.jpg",
+    href: "https://github.com/ron1232/outerx",
+    title: "OuterX",
+    description: "A small ecommerce site that I built back in 2021.",
+    technologies: ["Laravel", "PHP", "MySQL"],
+  },
+];
